6afe54c08dd0ef01947c77f373ace65ad9cbe354
Entyarr <domkve@gmail.com>
2022-12-04 12:18:09 +0200

    add day 4
    i REALLY dont like how i was forced to do this solution

---
+++ adventcode/day4/day4.py
@@ -0,0 +1,14 @@
+count = 0
+
+
+for line in open("wha.txt", 'r').readlines():
+    line = line.strip()
+    elves = line.split(',')
+    nums = []
+    for elf in elves:
+        nums.append(elf.split('-'))
+    if((int(nums[0][0]) >= int(nums[1][0]) and int(nums[0][1]) <= int(nums[1][1])) or
+      (int(nums[1][0]) >= int(nums[0][0]) and int(nums[1][1]) <= int(nums[0][1]))):
+        count += 1
+
+print(count)
